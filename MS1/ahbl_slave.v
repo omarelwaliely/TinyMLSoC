877834327f0d7ff48d09029be59d3ad4defd69b0
omarelwaliely <77898546+omarelwaliely@users.noreply.github.com>
2024-09-21 00:53:51 +0300

    added 3 registers to slave (needs modifications)
    Note, untested and needs modification of selection of register. As of right now it just selects based on the second hexadecimal number (from the left) in HADDR

--- MS1/ahbl_slave.v
+++ MS1/ahbl_slave.v
@@ -18,6 +18,11 @@ module ahbl_slave #(parameter ID = 32'hABCD_EF00) (
     reg [2:0]  HSIZE_d;
     reg        HWRITE_d;
     reg        HSEL_d;
+    reg [31:0] register_1_data;
+    reg [31:0] register_2_data;
+    reg [31:0] register_3_data;
+    reg[2:0] load;
+    
     always @(posedge HCLK or negedge HRESETn) begin
         if(HRESETn == 1'b0) begin
             HADDR_d     <= 0;
@@ -35,11 +40,43 @@ module ahbl_slave #(parameter ID = 32'hABCD_EF00) (
     end
 
     wire ahbl_we = HTRANS_d[1] & HSEL_d & HWRITE_d;
-    always @(posedge HCLK) begin
-        if(HRESETn & ahbl_we)
+   always @(posedge HCLK) begin
+        if (HRESETn & ahbl_we) begin
             $display("Slave %h: WRITE 0x%8x to 0x%8x", ID, HWDATA, HADDR_d);
+            case (HADDR_d[27:24])
+                4'd0: load <= 3'b100;
+                4'd1: load <= 3'b010;
+                4'd2: load <= 3'b001; 
+                default: load <= 3'b100;
+            endcase
+        end else begin
+            load <= 3'b000;
+        end
     end
 
+    register register_0(
+        .clk(HCLK),
+        .rst_n(HRESETn),
+        .load(load[0]),
+        .D(register_1_data),
+        .Q(HWDATA)
+    );
+
+    register register_1(
+        .clk(HCLK),
+        .rst_n(HRESETn),
+        .load(load[1]),
+        .D(register_2_data),
+        .Q(HWDATA)
+    );
+
+    register register_2(
+        .clk(HCLK),
+        .rst_n(HRESETn),
+        .load(load[2]),
+        .D(register_3_data),
+        .Q(HWDATA)
+    );
     assign HREADYOUT = 1;
     assign HRDATA = ID;
 endmodule
