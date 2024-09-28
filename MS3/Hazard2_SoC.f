470300c82cb55f8564b118676052d4f22c74fbdf
Omar Elwaliely <omarelwaliely@gmail.com>
2024-09-28 22:43:32 +0300

    milestone 3 omar added

---
+++ MS3/Hazard2_SoC.f
@@ -0,0 +1,7 @@
+hazard2_cpu.v 
+Hazard2.v 
+ahbl_splitter.v 
+ahbl_ram.v 
+ahbl_rom.v 
+ahbl_gpio.v
+Hazard2_SoC.v 
---
+++ MS3/test.c
@@ -0,0 +1,30 @@
+volatile unsigned int* gpio_data_A = (volatile unsigned int *) 0x40000000;
+volatile unsigned int* gpio_oe_A = (volatile unsigned int *) 0x40000004;
+volatile unsigned int* gpio_data_B = (volatile unsigned int *) 0x50000000;
+volatile unsigned int* gpio_oe_B = (volatile unsigned int *) 0x50000004;
+volatile unsigned int* gpio_data_C = (volatile unsigned int *) 0x60000000;
+volatile unsigned int* gpio_oe_C = (volatile unsigned int *) 0x60000004;
+
+
+
+
+int main() {
+
+    *gpio_oe_A = 0x00000000;
+    *gpio_oe_B = 0x00000000; 
+    *gpio_oe_C = 0xFFFFFFFF;
+    
+    unsigned int A_data = 0;
+    unsigned int B_data = 0;
+
+
+    while (1) { 
+
+        A_data = *gpio_data_A;
+        B_data = *gpio_data_B;
+        *gpio_data_C = A_data + B_data;
+        
+    }
+
+    return 0;
+}
