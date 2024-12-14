a9d3ae997ac881ecb5ed765953033a907bb8bd13
Omar Elwaliely <omarelwaliely@gmail.com>
2024-12-14 22:45:35 +0200

    tested fifo added i2s still not working

---
+++ Final_Omar/Hazard2_SoC_tb.v
@@ -0,0 +1,77 @@
+`timescale 1ns / 1ps
+
+module Hazard2_SoC_tb;
+
+    // Testbench signals
+    reg         HCLK;
+    reg         HRESETn;
+    reg en;
+    wire         I2S_in;     
+    wire        UART_TX;
+    wire [2:0]  LED_out;
+    wire        ws;
+    wire        i2s_clk;
+    wire [31:0] HADDR;
+    wire [2:0]  HSIZE;
+    wire [1:0]  HTRANS;
+    wire        HWRITE;
+    wire [31:0] HWDATA;
+    wire        HREADY;
+    wire [31:0] HRDATA;
+
+    // Clock generation
+    initial begin
+        HCLK = 0;
+    forever #3 HCLK = ~HCLK; // 6 MHz clock 
+    end
+
+    // Reset generation
+    initial begin
+        HRESETn = 0;
+        #20 HRESETn = 1;
+    end
+
+
+    i2s_test tb (
+        .WS(ws),
+        .BCLK(i2s_clk),
+        .DIN(I2S_in),
+        .en(en),
+        .rst_n(HRESETn)
+    );
+        Hazard2_SoC uut (
+        .HCLK(HCLK),
+        .HRESETn(HRESETn),
+        .I2S_in(I2S_in),
+        .UART_TX(UART_TX),
+        .LED_out(LED_out),
+        .ws(ws),
+        .i2s_clk(i2s_clk),
+        .HADDR(HADDR),
+        .HSIZE(HSIZE),
+        .HTRANS(HTRANS),
+        .HWRITE(HWRITE),
+        .HWDATA(HWDATA),
+        .HREADY(HREADY),
+        .HRDATA(HRDATA)
+    );
+
+    initial begin
+        HRESETn = 0;
+        #47;
+        @(posedge HCLK);
+        HRESETn = 1;
+    end
+    // Dump waveforms to a file
+    initial begin
+        $dumpfile("hazard2_soc_tb.vcd");
+        
+        $dumpvars(0, Hazard2_SoC_tb);  
+        wait(HRESETn == 1'b1);   
+        @(posedge HCLK);         
+        en = 1;    
+        #70000;  // Run simulation for 1000ns (1us) before stopping
+        $finish; // End the simulation
+    end
+
+endmodule
---
+++ Final_Omar/top.v
@@ -0,0 +1,39 @@
+module top (
+    output wire TX,
+    output wire [2:0] LED_RGB
+);
+    wire clk;
+    wire [2:0] LED_data;
+
+    SB_HFOSC #(
+    .CLKHF_DIV("0b11")  
+    ) u_SB_HFOSC (
+        .CLKHFPU(1'b1),
+        .CLKHFEN(1'b1),
+        .CLKHF(clk)
+    );
+
+
+    Hazard2_SoC SoC(
+    .HCLK(clk),
+    .UART_TX(TX),
+    .LED_out(LED_data)
+    );
+
+	SB_RGBA_DRV #(
+		.CURRENT_MODE("0b1"),
+		.RGB0_CURRENT("0b000111"),
+		.RGB1_CURRENT("0b000111"),
+		.RGB2_CURRENT("0b000111")
+	) rgb_drv_I (
+		.RGBLEDEN(1'b1),
+		.RGB0PWM(LED_data[0]),
+		.RGB1PWM(LED_data[1]),
+		.RGB2PWM(LED_data[2]),
+		.CURREN(1'b1),
+		.RGB0(LED_RGB[0]),
+		.RGB1(LED_RGB[1]),
+		.RGB2(LED_RGB[2])
+	);
+
+endmodule
